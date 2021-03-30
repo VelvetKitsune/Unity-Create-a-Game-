f59f28e569190e73540ab3d556556508c568ddce
VelvetKitsune <Velvetkitsune@mail.com>
2021-03-30 16:20:19 +1100

    Following episode 23 of Sebastian's tutorials!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -64,4 +64,10 @@ public class Player : LivingEntity {
 			gunController.Reload();
 		}
     }
+	
+	public override void Die () {
+		AudioManager.instance.PlaySound ("Player Death", transform.position);
+		base.Die ();
+	}
+	
 }
